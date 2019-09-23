6d455eb4b389fcffc74eda675077509dbe1d7c75
Yevgen <notky@ukr.net>
2019-09-23 13:34:31 +0300

    add coments

--- tabs/js/script.js
+++ tabs/js/script.js
@@ -11,23 +11,32 @@ window.addEventListener('DOMContentLoaded', function(){
         function displayTabs(a){
             for(let i = 0; i < contents.length; i++){
                 if(a == i){
-                    showTab(i);
+                    showTab(i); // показываем вкладку
                 } else {
-                    hideTab(i);
+                    hideTab(i); // прячем вкладку
                 }
             }
         }
     
+        /**
+         * Отображение вкладки
+         * @param {index} a - индекс вкладки
+         */
         function showTab(a){
             contents[a].classList.add("show");
             contents[a].classList.remove("hide");
         }
     
+        /**
+         * Прячем вкладку
+         * @param {index} a 
+         */
         function hideTab(a){
             contents[a].classList.remove("show");
             contents[a].classList.add("hide");
         }
     
+        // Прячем все вкладки кроме первой, так как только загружен документ
         displayTabs(0);
     
         wrapper.addEventListener('click', function(event){
@@ -42,7 +51,8 @@ window.addEventListener('DOMContentLoaded', function(){
         });
     }
 
-    // Использование функции
+    // Использование:
+
     // start
     let tab = document.querySelector(".info"),
         tabs = document.querySelectorAll(".info-header-tab"),
